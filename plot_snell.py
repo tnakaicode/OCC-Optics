f001128fe277f80752c6c6d08d5626f6170df7ee
Taku Nakai <tnakaicode@gmail.com>
2019-10-12 22:15:02 +0900

    plot test

--- plot_snell.py
+++ plot_snell.py
@@ -7,19 +7,39 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 class OpticsTraject (object):
 
-    def __init__(self):
-        self.cff_x = 0.1
+    def __init__(self, xy=[-50, -50], th=45):
+        self.cff_x = 2.0
         self.rng_x = 10.0
-        self.cff_y = 2.0
-        self.rng_y = 1.5
+        self.cff_y = 1.5
+        self.rng_y = 20.0
+        self.initialize(xy, th)
 
-        self.xy0 = [0, 0]
-        self.th0 = 0.0
-        # self.vc0 = [self.refract_2d(r_0[0]) * np.cos(theta_0),
-        #            self.refract_2d(r_0[0]) * np.sin(theta_0)]
+    def diff_func(self, dat, t):
+        # Compute the differential
+        pos = [dat[0], dat[1]]
+        vec = [dat[2], dat[3]]
+        grd = nd.Gradient(self.refract)(pos)
+        ref = self.refract(pos)
+        #self.pos = [self.vec[0], self.vec[1]]
+        #self.vec = [grd[0] * ref, grd[1] * ref]
+        return vec + [grd[0] * ref, grd[1] * ref] + [ref]
 
-    def initialize(self, xy=[0,0], th=0):
-        print("ok")
+    def diff_run(self, t=[0, 50, 0.01]):
+        self.t_range = np.arange(*t)
+        ini_dat = self.pos + self.vec + [self.ref]
+        dat = odeint(self.diff_func, ini_dat, self.t_range)
+        self.dat = np.array(dat)
+        print(self.dat)
+
+    def initialize(self, xy=[0, 0], th=0):
+        self.pos = [xy[0], xy[1]]
+        self.rad = np.deg2rad(th)
+
+        self.ref = self.refract_2d(*self.pos)
+        self.vec = [
+            self.ref * np.cos(self.rad),
+            self.ref * np.sin(self.rad)
+        ]
 
     def refract_2d(self, x=0, y=0):
         ref_x = self.cff_x - (self.cff_x - 1) / \
@@ -28,17 +48,11 @@ class OpticsTraject (object):
             (1 + np.exp(-(y - self.cff_y**2) / self.rng_y))
         return ref_x + ref_y
 
-    def refract(self, r):
+    def refract(self, xy=[0, 0]):
         # Refractive index function
-        return self.refract_2d(*r)
+        return self.refract_2d(*xy)
 
-    def diff_y(self, y, t):
-        # Compute the differential
-        grd = nd.Gradient(self.refract)([y[0], y[1]])
-        n_t = self.refract([y[0], y[1]])
-        return [y[2], y[3], grd[0] * n_t, grd[1] * n_t]
-
-    def plot_2d(self):
+    def plot_2d(self, dirname="./", pngname="plot_snell"):
         sx, sy = 50.0, 50.0
         nx, ny = 100, 100
         px = np.linspace(-1, 1, nx) * 100
@@ -53,7 +67,7 @@ class OpticsTraject (object):
         fig, ax = plt.subplots()
         divider = make_axes_locatable(ax)
         ax.set_aspect('equal')
-        
+
         ax_x = divider.append_axes("bottom", 1.0, pad=0.5, sharex=ax)
         ax_x.plot(mesh[0][mx, :], func[mx, :])
         ax_x.set_title("y = {:.2f}".format(sy))
@@ -63,11 +77,37 @@ class OpticsTraject (object):
         ax_y.set_title("x = {:.2f}".format(sx))
 
         im = ax.contourf(*mesh, func, cmap="jet")
+        ax.plot(self.dat[:, 0], self.dat[:, 1])
         plt.colorbar(im, ax=ax, shrink=0.9)
 
-        plt.savefig("./plot_snell.png")
+        pngfile = dirname + pngname + ".png"
+        plt.savefig(pngfile)
+
+        plt.subplot(211)
+        plt.plot(self.t_range, self.dat[:, 0])
+
+        plt.subplot(212)
+        plt.plot(self.t_range, self.dat[:, 1])
+
+        pngfile = dirname + pngname + "_xy.png"
+        plt.savefig(pngfile)
+
+        plt.subplot()
+        plt.plot(self.t_range, self.dat[:, -1])
+
+        pngfile = dirname + pngname + "_ref.png"
+        plt.savefig(pngfile)
 
 
 if __name__ == '__main__':
     obj = OpticsTraject()
+    obj.diff_run()
     obj.plot_2d()
+
+    obj.initialize(xy=[-50, -50], th=30)
+    obj.diff_run(t=[0, 50, 0.1])
+    obj.plot_2d(pngname="plot_snell01")
+
+    obj.initialize(xy=[-50, -50], th=30)
+    obj.diff_run(t=[0, 50, 0.01])
+    obj.plot_2d(pngname="plot_snell02")
