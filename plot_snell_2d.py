63f01454a38c7e3a84871f0a6dd89c72ed71ebce
Taku Nakai <tnakaicode@gmail.com>
2019-10-12 19:48:58 +0900

    format

--- plot_snell_2d.py
+++ plot_snell_2d.py
@@ -4,6 +4,12 @@ import numdifftools as nd
 from scipy.integrate import odeint
 
 
+class OpticsTraject (object):
+
+    def __init__(self):
+        print("ok")
+
+
 def refract_1d(x=0):
     cff = 2
     rng = 0.8
@@ -33,26 +39,26 @@ def diff_y(y, t):
     return [y[2], y[3], grd[0] * n_t, grd[1] * n_t]
 
 
-r_0 = [0, 4]  # initial position
-theta_0 = np.pi / 6  # initial angle
+if __name__ == '__main__':
+    r_0 = [0, 4]  # initial position
+    theta_0 = np.pi / 6  # initial angle
 
-v_0 = [refract_1d(r_0[0]) * np.cos(theta_0),
-       refract_1d(r_0[0]) * np.sin(theta_0)]
+    v_0 = [refract_1d(r_0[0]) * np.cos(theta_0),
+           refract_1d(r_0[0]) * np.sin(theta_0)]
 
+    # Integration
+    t_range = np.arange(0, 10, 0.01)
+    sol = odeint(diff_y, r_0 + v_0, t_range)
 
-# Integration
-t_range = np.arange(0, 10, 0.01)
-sol = odeint(diff_y, r_0 + v_0, t_range)
+    # Plotting the path
+    plt.plot(sol[:, 0], sol[:, 1], 'y', linewidth=2)
 
-# Plotting the path
-plt.plot(sol[:, 0], sol[:, 1], 'y', linewidth=2)
+    # Plotting function n
+    X, Y = np.mgrid[0:20:1000j, 0:20:1000j]
+    pcm = plt.pcolormesh(X, Y, n([X, Y]), cmap='jet', vmin=1)
+    cbar = plt.colorbar(pcm)
+    cbar.ax.set_ylabel("n  Refractive index")
 
-# Plotting function n
-X, Y = np.mgrid[0:20:1000j, 0:20:1000j]
-pcm = plt.pcolormesh(X, Y, n([X, Y]), cmap='jet', vmin=1)
-cbar = plt.colorbar(pcm)
-cbar.ax.set_ylabel("n  Refractive index")
-
-plt.xlabel("cm")
-plt.ylabel("cm")
-plt.show()
+    plt.xlabel("cm")
+    plt.ylabel("cm")
+    plt.show()
